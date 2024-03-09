bb10473f533abec9c98f7a6fce2665b61223259e
Saikat Kar <ksaikat009@gmail.com>
2024-03-09 21:10:53 +0530

    created flights create crud

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,11 @@
 const express =require('express');
 const CityController = require('../../controllers/city-controller');
 const AirportController = require('../../controllers/airport-controller');
+const FlightController = require('../../controllers/flight-controller');
+
 
 const router = express.Router();
+
 router.post('/city',CityController.create);
 router.delete('/city/:id',CityController.destroy);
 router.get('/city/:id',CityController.get);
@@ -15,4 +18,5 @@ router.delete('/airport/:id',AirportController.destroy);
 router.get('/airport/:id',AirportController.get);
 router.patch('/airport/:id',AirportController.update);
 
+router.post('/flights', FlightController.create);
 module.exports = router;
